7cdbfef425f4e5e73b59b99e6e895b6f945feb11
tangkb <tangkb_04872>
2018-10-29 19:55:15 +0800

    初始化问卷项目

---
+++ src/main/java/com/key/common/base/dao/UserDao.java
@@ -0,0 +1,17 @@
+package com.key.common.base.dao;
+
+import com.key.common.base.entity.User;
+import com.key.common.dao.BaseDao;
+
+
+/**
+ * @author keyuan(keyuan258@gmail.com)
+ *
+ * https://github.com/wkeyuan/DWSurvey
+ * http://dwsurvey.net
+ */
+public interface UserDao extends BaseDao<User, String> {
+
+	public void resetUserGroup(String groupId);
+	
+}
---
+++ src/main/java/com/key/common/utils/DiaowenProperty.java
@@ -0,0 +1,59 @@
+package com.key.common.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import com.key.common.utils.web.Struts2Utils;
+import org.apache.struts2.ServletActionContext;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+
+import com.baidu.ueditor.ConfigManager;
+import com.sun.org.apache.xml.internal.security.Init;
+
+import javax.servlet.ServletContext;
+
+/**
+ * 
+ * @author keyuan
+ *
+ */
+public class DiaowenProperty extends
+		PropertyPlaceholderConfigurer {
+
+	public static String STORAGE_URL_PREFIX = null;
+
+
+//	private static Map<String, String> ctxPropertiesMap;
+
+	@Override
+	protected void processProperties(
+			ConfigurableListableBeanFactory beanFactoryToProcess,
+			Properties props) throws BeansException {
+		super.processProperties(beanFactoryToProcess, props);
+		
+		/*
+		ctxPropertiesMap = new HashMap<String, String>();
+		for (Object key : props.keySet()) {
+			String keyStr = key.toString();
+			String value = props.getProperty(keyStr);
+			ctxPropertiesMap.put(keyStr, value);
+		}
+		*/
+	}
+/*
+	public static String getContextProperty(String name) {
+	    return ctxPropertiesMap.get(name);
+	}
+*/
+	public void diaowenInit(){
+		System.out.println("做点有意义事情。。。");
+		System.out.println(ServletActionContext.getContext());
+	}
+
+}
